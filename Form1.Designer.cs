d87215cc0220f258ba7ce5642a611754a85c0051
Aaron Audet <aaafrench@gmail.com>
2021-02-07 18:07:15 -0500

    base

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Lab5A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.colorPick = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.flowRate = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorPick
+            // 
+            this.colorPick.Location = new System.Drawing.Point(43, 12);
+            this.colorPick.Name = "colorPick";
+            this.colorPick.Size = new System.Drawing.Size(75, 23);
+            this.colorPick.TabIndex = 0;
+            this.colorPick.Text = "Color";
+            this.colorPick.UseVisualStyleBackColor = true;
+            this.colorPick.Click += new System.EventHandler(this.colorPick_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(136, 12);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Close";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(43, 87);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab5A.Properties.Resources.Faucet;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 61);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(329, 437);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.colorPick);
+            this.Name = "Form1";
+            this.Text = "A Drop in the Bucket";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colorPick;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer flowRate;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+ * Name: Aaron Audet
+ * Student Number: 000367464
+ * File Date: December 11, 2020
+ * Program purpose: The purpose of this program is to display a bucket filling with paint of a color you chose
+ * Statement of Authorship: I, Aaron Audet, 000367464 certify that this material is my original work.  No other person's work has been used without due acknowledgement.
+ */
+
+namespace Lab5A
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;             // Allow Graphics
+        private Pen p;                  // Used for making the bucket and the paint
+        private SolidBrush b;           // This brush is for adding paint
+        private SolidBrush bReset;      // This brush is for removing paint
+        private Color c = Color.White;  // Makes default color white
+        private int height = 399;       // Makes default paint height
+        private bool reset = false;     // This is to tell us to clear the paint
+
+        /// <summary>
+        /// Sets the default configurations
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(form1_Paint);
+            flowRate.Tick += new EventHandler(tick);
+        }
+
+        /// <summary>
+        /// Sets default vaules and draws the container
+        /// </summary>
+        /// <param name="sender">Not used</param>
+        /// <param name="e">Not Used</param>
+        private void form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Set values
+            g = e.Graphics;
+            p = new Pen(c);
+            b = new SolidBrush(c);
+            bReset = new SolidBrush(Color.Black);
+
+            // Draw container
+            g.DrawLine(p, 100, 290, 100, 400);
+            g.DrawLine(p, 100, 400, 300, 400);
+            g.DrawLine(p, 300, 400, 300, 290);
+        }
+
+        /// <summary>
+        /// Exits the application
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Handles changing the color
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void colorPick_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = c;
+            colorDialog.ShowDialog();
+            c = colorDialog.Color;
+        }
+
+        /// <summary>
+        /// This handles any changes in the trackbar to change flow rate
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if(reset)                   // If bucket is full, remove paint
+            {
+                g = this.CreateGraphics();
+
+                g.FillRectangle(bReset, 101, 300, 199, 100);
+                reset = false;
+                height = 399;
+            }
+            
+            if(trackBar1.Value == 0)    // If tap is off, stop tap and remove paint flow
+            {
+                flowRate.Enabled = false;
+                g.FillRectangle(bReset, 125, 205, 20, height - 204);
+            } else                      // Start timer and change flow rate
+            {
+                flowRate.Enabled = true;
+                flowRate.Interval = 100 - (trackBar1.Value - 1) * (50 / 9);
+            }
+        }
+
+        /// <summary>
+        /// This method will fill the bucket every event call
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void tick(object sender, EventArgs e)
+        {
+            if (height < 300) // If bucket is full
+            {
+                g.FillRectangle(bReset, 125, 205, 20, height - 204);
+                flowRate.Enabled = false;
+                trackBar1.Value = 0;
+                reset = true;
+            } else           // If not then fill
+            {
+                g = this.CreateGraphics();
+
+                g.DrawLine(p, 101, height, 299, height);        // Paint
+                g.FillRectangle(b, 125, 205, 20, height - 204); // Tap
+                height--;
+            }
+            
+        }
+    }
+}
